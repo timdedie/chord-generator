2a781d46aa19f88558c8d7a99a6fb073312980f7
timdedie <tim.dedie@student.uibk.ac.at>
2025-06-14 20:20:21 +0200

    blog date

--- lib/blogData.ts
+++ lib/blogData.ts
@@ -11,7 +11,7 @@ export const posts: Post[] = [
         slug: 'midi-chord-generator-for-producers',
         title: 'How a MIDI Chord Generator Can Break Your Writer\'s Block',
         description: 'Staring at a blank piano roll? Discover how a MIDI chord generator can instantly spark new ideas, accelerate your workflow, and help you learn music theory along the way.',
-        date: '2023-10-26', // Use today's date
+        date: '2025-06-14',
     },
     // Add your next blog post here...
 ];
